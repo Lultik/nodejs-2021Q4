085189d561cb0817d83b70c6b85ce82dc72fc413
Mikhail Stankevich <Mikhail_Stankevich@epam.com>
2021-11-21 21:42:32 +0300

    refactor: separate stream callbacks to pure functions

--- chipering-cli-tool/services/customReadableStream.js
+++ chipering-cli-tool/services/customReadableStream.js
@@ -6,28 +6,55 @@ class CustomReadableStream extends Readable {
   constructor(inputPath) {
     super();
     this.path = path.resolve(__dirname + `/../${inputPath}`);
-    this.index = 0;
-
-
+    this.fd = null;
   }
 
-  _read(number) {
+  openCallback (err, fd, done) {
+    if (err) {
+      done(err);
+      console.error(err.message)
+      process.exit(1);
+    } else {
+      this.fd = fd;
+      done();
+    }
+  }
+
+  _construct(done) {
+    fs.open(this.path, 'r', (err, fd) => this.openCallback(err, fd, done));
+  }
+
+  readCallback(err, data, size) {
+    if (err) {
+      console.error(`${this.path} is not readable`);
+      this._destroy(err);
+    }
+
     try {
-      fs.readFile(this.path, 'utf-8', (err, data) => {
-        if (err) {
-          console.error(`${this.path} ${err ? 'is not readable' : 'is readable'}`);
-          process.exit(1);
-        } else {
-          if (this.index < data.length) {
-            this.push(data.slice(this.index, this.index + number));
-            this.index += number;
-          } else {
-            this.push(null);
-          }
-        }
-      })
-    } catch (err) {
+      if (this.index < data.length) {
+        this.push(data.slice(this.index, this.index + size));
+        this.index += size;
+      } else {
+        this.push = null;
+      }
+    } catch (e) {
       console.error(err.message);
+      this._destroy(err);
+    }
+  }
+
+  _read(size) {
+    fs.readFile(this.path, 'utf-8', (err, data) => this.readCallback(err, data, size))
+  }
+
+  closeCallback (e) {
+    process.exit(1);
+  }
+
+  _destroy(err, callback) {
+    if (this.fd) {
+      fs.close(this.fd, (e) => this.closeCallback(e));
+    } else {
       process.exit(1);
     }
   }
